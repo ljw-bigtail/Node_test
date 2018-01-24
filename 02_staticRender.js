941d92c1ed786fce515c7697ecee040cc6e28a00
ljw-bigtail <747624075@qq.com>
2018-01-24 09:19:49 +0800

    01、02

---
+++ 02_staticRender.js
@@ -0,0 +1,37 @@
+// 引入包
+var http = require('http');
+var fs = require('fs');
+
+// 创建服务器
+// 表示有请求进来需要做的事
+var server = http.createServer(function (req, res) {
+    // req require
+    // res response
+    // 状态码、文件类型、字符集
+    if (req.url == '/a') {
+        fs.readFile('./1.html', (err, data) => {
+            res.writeHead(200, {
+                'Content-type': 'text/html;charset=UTF-8'
+            });
+            res.end('Hello World' + data);
+        });
+    } else if (req.url == '/b') {
+        fs.readFile('./2.html', (err, data) => {
+            res.writeHead(200, {
+                'Content-type': 'text/html;charset=UTF-8'
+            });
+            res.end(data);
+        });
+    } else if (req.url == '/1.jpg') {
+        fs.readFile('./1.jpg', (err, data) => {
+            res.end(data);
+        });
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html;charset=UTF-8'
+        });
+        res.end('找不到了');
+    }
+});
+// 运行服务器来某一端口
+server.listen(3000, '127.0.0.1');
